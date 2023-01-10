91f531658f6b562475c38cdfbd0f649c8295c7b5
Sujithkharvi <Admin@DESKTOP-44DNH9H>
2023-01-10 09:48:37 +0530

    Add project files.

---
+++ fourthAssignment/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using fourthAssignment;
+
+StringClass obj = new StringClass();
+obj.PrintMethod();
+Console.WriteLine(obj.ToCurrency("123"));
+Console.WriteLine(obj.StringContainsNumber("12345"));
---
+++ fourthAssignment/StringClass.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fourthAssignment
+{
+    public  class StringClass
+    {
+        public void PrintMethod()
+        {
+            Console.WriteLine("This is from class Demo printMethod");
+        }
+    }
+
+    public static class ExtensionClass
+    {
+        public static string ToCurrency(this StringClass obj,string inputString)
+        {
+            try
+            {
+                int isNumber = Convert.ToInt32(inputString);
+                string outputString = "$" + isNumber;
+                return outputString;
+            }
+            catch(Exception ex)
+            {
+                throw;
+            }
+            
+        }
+
+        public static string StringContainsNumber(this StringClass obj, string inputString)
+        {
+           /* bool isIntString = inputString.Any(char.IsDigit);
+            if (isIntString) return "$" + inputString;
+            throw new FormatException("Input string format is Invalid");*/
+
+            bool isInt = inputString.All(char.IsDigit);
+            if (isInt) return "$" + inputString;
+            throw new FormatException("Input string format is Invalid");
+
+        }
+    }
+}
